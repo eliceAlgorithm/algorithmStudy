c8cd0111690c6725f43f3c9aeafe6671a16aae43
leeleeleeleejun <wnstjr6293@naver.com>
2023-04-07 18:52:53 +0900

    [DFS, BFS] 타겟넘버, 여행경로

--- 이준석/소수찾기.js
+++ 이준석/소수찾기.js
@@ -1,4 +1,6 @@
 function solution(numbers) {
+  // 숫자의 조합이 들어갈 객체
+  //
   const set = new Set();
   var answer = 0;
   numbers = numbers.split("");
@@ -17,17 +19,25 @@ function solution(numbers) {
 
   recursive([], numbers);
 
-  for (const item of [...set]) {
-    // 모든 조합을 순회하며 소수 확인
-    if (isPrime(item)) answer++;
-  }
+  // 재귀함수가 불러지는 과정
+  // 1. recursive([], [1, 2, 3])
+  // 2. recursive([1], [2, 3]) , ///// recursive([2], [1, 3]) , recursive([3], [2, 3])
+  // 3. recursive([1, 2], [3]) , recursive([1, 3], [2])
+  // 4. recursive([1, 2, 3], []) , recursive([1, 3, 2], [])
+
   // 소수를 구하는 함수
   function isPrime(num) {
     if (num === 0 || num === 1) return false;
+    // 에라토스테네스의 채
     for (let i = 2; i <= Math.sqrt(num); i++) {
       if (num % i === 0) return false;
     }
     return true;
+  }
+
+  for (const item of [...set]) {
+    // 모든 조합을 순회하며 소수 확인
+    if (isPrime(item)) answer++;
   }
 
   return answer;
